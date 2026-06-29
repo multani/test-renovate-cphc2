5fd0c1a50dd5a3ec854ac9646ec4297019a8da8c
Jonathan Ballet <jon@multani.info>
2026-06-29 18:45:12 +0200

    message topic

--- 8.9-alpha/install.sh
+++ 8.9-alpha/install.sh
@@ -3,6 +3,5 @@
 set -o errexit -o nounset -o pipefail
 IFS=$'\n\t'
 
-# renovate: datasource=github-tags depName=camunda/camunda-platform-helm versioning=regex:^camunda-platform-8\.9-(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)$
-git_tag="camunda-platform-8.9-14.0.0-alpha4"
+# renovate: version=camunda-platform-8.9
 helm_chart_version="14.0.0-alpha4"
