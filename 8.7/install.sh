f3475e4fc3382096fb4b02e0c92938ae6c8eee03
Jonathan Ballet <jon@multani.info>
2026-06-29 18:49:06 +0200

    fix

--- 8.7/install.sh
+++ 8.7/install.sh
@@ -3,6 +3,5 @@
 set -o errexit -o nounset -o pipefail
 IFS=$'\n\t'
 
-# renovate: datasource=github-tags depName=camunda/camunda-platform-helm versioning=regex:^camunda-platform-8\.7-(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)?(-alpha(?<prerelease>.*))?$
-git_tag="camunda-platform-8.7-12.8.6"
+# renovate: version=camunda-platform-8.7
 helm_chart_version="12.8.6"
--- 8.9/install.sh
+++ 8.9/install.sh
@@ -4,6 +4,5 @@ set -o errexit -o nounset -o pipefail
 IFS=$'\n\t'
 
 
-# renovate: datasource=github-tags depName=camunda/camunda-platform-helm versioning=regex:^camunda-platform-8\.9-(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)?(-alpha(?<prerelease>.*))?$
-git_tag="camunda-platform-8.9-14.4.0"
+# renovate: version=camunda-platform-8.9
 helm_chart_version="14.4.0"
